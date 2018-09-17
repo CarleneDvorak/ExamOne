c058a66fba18166058a2f5a6488821702b699a92
DVORAK <cd0884213@otc.edu>
2018-09-17 14:13:21 -0500

    almost

--- Exam.js
+++ Exam.js
@@ -1,17 +1,24 @@
  const shoppingCart = {
     fName: "Carlene",
-    lName: "Dvorak"}
+    lName: "Dvorak",
+    items:  ["Shampoo", "Conditioner", "Hairspray"],
+    price: [8.50, 7.00, 5.25],
 
-const items = ["Shampoo", "Conditioner", "Hairspray"];
-const price = ["$8.50", "$7.00", "$5.25"];
+}
+
 
  function  printShoppingCart(){
-return `Name: ${shoppingCart.fName} ${shoppingCart.lName}
-        Items Purchases:`;
-       `${items} ${price}`;}
+return `Name: ${shoppingCart.fName} ${shoppingCart.lName} 
+Items Purchases:
+${shoppingCart.items[0]}  $${shoppingCart.price[0]} 
+${shoppingCart.items[1]}  $${shoppingCart.price[1]} 
+${shoppingCart.items[2]}  $${shoppingCart.price[2]}`
+
+}
 
 function totalPrice(){
-    return `${price[0]} + ${price[1]}+ ${price[2]}`;
+    return  `Total Purchase: $`  +
+    (shoppingCart.price[0]  + shoppingCart.price[1] + shoppingCart.price[2])
 }
 
 console.log(printShoppingCart());
