5ef69bbbf9aa299e3bc723e64a2d133c41f8d6d9
DVORAK <cd0884213@otc.edu>
2018-09-18 11:25:01 -0500

    transition

--- Exam.js
+++ Exam.js
@@ -3,24 +3,36 @@
     lName: "Dvorak",
     items:  ["Shampoo", "Conditioner", "Hairspray"],
     price: [8.50, 7.00, 5.25],
-
 }
 
+function printShoppingCart() {
+    String: words = "";
+    for (i=0; i<shoppingCart.items.length && i<shoppingCart.price.length; i++){
+        words = (words + shoppingCart.items[i] + 
+            " " + shoppingCart.price[i] + "                                                                ");
+        
+     } 
+    
+     /*(
+    ${shoppingCart.items[0]}  $${shoppingCart.price[0]} 
+    ${shoppingCart.items[1]}  $${shoppingCart.price[1]} 
+    ${shoppingCart.items[2]}  $${shoppingCart.price[2]}`)
+    */
 
- function  printShoppingCart(){
-return `Name: ${shoppingCart.fName} ${shoppingCart.lName} 
-Items Purchases:
-${shoppingCart.items[0]}  $${shoppingCart.price[0]} 
-${shoppingCart.items[1]}  $${shoppingCart.price[1]} 
-${shoppingCart.items[2]}  $${shoppingCart.price[2]}`
-
+    words = (`Name:${shoppingCart.fName} ${shoppingCart.lName} 
+Items Purchases:   
+` + words);
+    return words;
 }
 
-function totalPrice(){
-    return  `Total Purchase: $`  +
-    (shoppingCart.price[0]  + shoppingCart.price[1] + shoppingCart.price[2])
+function totalPrice(){ 
+    var cost = 0;
+    for (i=0; i<shoppingCart.price.length; i++){
+        cost = cost + shoppingCart.price[i];
+    }
+    cost = (`Total Purchase: $` + cost);
+    return cost;  
 }
 
-console.log(printShoppingCart());
+console.log( printShoppingCart());
 console.log(totalPrice());
-
