356f44a343622d148a9cdbb40e88e60772a6fbb6
DVORAK <cd0884213@otc.edu>
2018-09-20 10:23:01 -0500

    complete

--- Exam.js
+++ Exam.js
@@ -2,19 +2,24 @@
     fName: "Carlene",
     lName: "Dvorak",
     items:  ["Shampoo", "Conditioner", "Hairspray"],
-    price: [8.50, 7.00, 5.25],
+    price: [8.50, 7.00, 5.00],
 
     //this is a function inside an object
     printShoppingCart(){
-        const cart = `Name: ${shoppingCart.fName} ${shoppingCart.lName}
-       
-        Items Purchases $`
+        var total= 0;
+        let cost='';  
+            for (let i = 0; i <= this.items.length - 1; i ++){
+        cost = cost +`${this.items[i]}  $${this.price[i]} \n` ;                  
+            
+        }
+        for (let i = 0; i <this.price.length; i++){
+            total = this.price[i] + total;
+        }
+return `Name: ${this.fName} ${this.lName}
+Items Purchases:  
+${cost}Total Purchase: $${total}`;
+    }
+}
+    console.log(shoppingCart.printShoppingCart());
+ 
         
-        var i;
-        for (i = 0; i <= this.items.length -1; i ++){
-        console.log; {shoppingCart.items[i]};
-        
-            return `cart`;}
-       
-           
-      console.log (shoppingCart.printShoppingCart());}}
