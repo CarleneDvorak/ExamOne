e7786db5077b78407175814f41617962f4bd680a
CarleneDvorak <42741569+CarleneDvorak@users.noreply.github.com>
2018-09-20 06:18:30 -0500

    Just About

--- Exam.js
+++ Exam.js
@@ -3,36 +3,18 @@
     lName: "Dvorak",
     items:  ["Shampoo", "Conditioner", "Hairspray"],
     price: [8.50, 7.00, 5.25],
-}
 
-function printShoppingCart() {
-    String: words = "";
-    for (i=0; i<shoppingCart.items.length && i<shoppingCart.price.length; i++){
-        words = (words + shoppingCart.items[i] + 
-            " " + shoppingCart.price[i] + "                                                                ");
+    //this is a function inside an object
+    printShoppingCart(){
+        const cart = `Name: ${shoppingCart.fName} ${shoppingCart.lName}
+       
+        Items Purchases $`
         
-     } 
-    
-     /*(
-    ${shoppingCart.items[0]}  $${shoppingCart.price[0]} 
-    ${shoppingCart.items[1]}  $${shoppingCart.price[1]} 
-    ${shoppingCart.items[2]}  $${shoppingCart.price[2]}`)
-    */
-
-    words = (`Name:${shoppingCart.fName} ${shoppingCart.lName} 
-Items Purchases:   
-` + words);
-    return words;
-}
-
-function totalPrice(){ 
-    var cost = 0;
-    for (i=0; i<shoppingCart.price.length; i++){
-        cost = cost + shoppingCart.price[i];
-    }
-    cost = (`Total Purchase: $` + cost);
-    return cost;  
-}
-
-console.log( printShoppingCart());
-console.log(totalPrice());
+        var i;
+        for (i = 0; i <= this.items.length -1; i ++){
+        console.log; {shoppingCart.items[i]};
+        
+            return `cart`;}
+       
+           
+      console.log (shoppingCart.printShoppingCart());}}
